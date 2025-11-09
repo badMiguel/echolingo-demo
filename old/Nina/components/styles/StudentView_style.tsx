66eca991dd0aa58793c9074b78165c1080a37439
MiguelBad <badayosbiboy@gmail.com>
2025-11-09 21:39:57 +0930

    removed old

--- old/Nina/components/styles/StudentView_style.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-const styles = StyleSheet.create({
-    student_container: {
-        flex: 1,
-        padding: 20,
-    },
-});
-export default styles;
